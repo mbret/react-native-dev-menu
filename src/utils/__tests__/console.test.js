ae34b8d2bd47953a541b349efaafedfa24d92469
Maxime Bret <xmax54@gmail.com>
2018-06-29 22:55:04 +0200

    Revert "test coverage"
    This reverts commit 14025b7eadffa010f1a49392db0819c19ae81a53.

--- src/utils/__tests__/console.test.js
+++ src/utils/__tests__/console.test.js
@@ -6,9 +6,9 @@ describe('.warn()', () => {
   test('no crash', () => {
     expect(warn('foo')).toBe(undefined)
 
-    // console.warn = null
-    // expect(warn('foo')).toBe(undefined)
-    // console.warn = _warn
+    console.warn = null
+    expect(warn('foo')).toBe(undefined)
+    console.warn = _warn
   })
 })
 
